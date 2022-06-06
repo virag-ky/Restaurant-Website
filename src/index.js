2640a21ece5befc3f869bfb33796b2adb1d65912
Zero to Mastery <vkormoczy@gmail.com>
2022-06-06 16:43:51 +0300

    Add navbar and event listener to nav items

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 import "./styles/style.scss";
+
+const sectionContainer = document.getElementById("content");
+
+window.addEventListener("DOMContentLoaded", () => {
+  const navBar = document.createElement("nav");
+  navBar.innerHTML = `<ul>
+    <li id="home">Home</li>
+    <li id="menu">Menu</li>
+    <li id="contact">Contact</li>
+  </ul>`;
+  document.body.insertBefore(navBar, sectionContainer);
+
+  const navList = [...document.querySelectorAll("li")];
+  navList.forEach((item) =>
+    item.addEventListener("click", (e) => {
+      switch (e.target.id) {
+        case "home":
+          console.log("home");
+          break;
+        case "menu":
+          console.log("menu");
+          break;
+        case "contact":
+          console.log("contact");
+          break;
+        default:
+          console.log("home");
+          break;
+      }
+    })
+  );
+});
