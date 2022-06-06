9a0de7a831eb04f0932a36d38129f6bb487c973e
Zero to Mastery <vkormoczy@gmail.com>
2022-06-06 15:03:43 +0300

    Setup project with webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      { test: /\.txt$/, use: "raw-loader" },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/restaurant.html" })],
+};
