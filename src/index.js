97ec209c96d34fef196f6076074f0045f6fbe9f4
Zero to Mastery <vkormoczy@gmail.com>
2022-06-07 18:38:47 +0300

    Fix linter issues

--- src/index.js
+++ src/index.js
@@ -1,31 +1,27 @@
-import "./styles/style.scss";
-import createHomePage from "./modules/homepage.js";
-import createNavBar from "./modules/navBar.js";
+import './styles/style.scss';
+import createHomePage from './modules/homepage.js';
+import createNavBar from './modules/navBar.js';
 
-const sectionContainer = document.getElementById("content");
-
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener('DOMContentLoaded', () => {
   createNavBar();
   createHomePage();
 
-  const homePage = document.getElementById("home-page");
-  homePage.classList.remove("hide");
+  const homePage = document.getElementById('home-page');
+  homePage.classList.remove('hide');
 
-  const navList = [...document.querySelectorAll("li")];
-  navList.forEach((item) =>
-    item.addEventListener("click", (e) => {
-      switch (e.target.id) {
-        case "home":
-          homePage.classList.remove("hide");
-          break;
-        case "menu":
-          break;
-        case "contact":
-          break;
-        default:
-          homePage.classList.remove("hide");
-          break;
-      }
-    })
-  );
+  const navList = [...document.querySelectorAll('li')];
+  navList.forEach((item) => item.addEventListener('click', (e) => {
+    switch (e.target.id) {
+      case 'home':
+        homePage.classList.remove('hide');
+        break;
+      case 'menu':
+        break;
+      case 'contact':
+        break;
+      default:
+        homePage.classList.remove('hide');
+        break;
+    }
+  }));
 });
--- src/modules/navBar.js
+++ src/modules/navBar.js
@@ -1,32 +1,32 @@
 const createNavBar = () => {
-  const nav = document.createElement("nav");
+  const nav = document.createElement('nav');
   nav.innerHTML = `<ul>
   <li id="home">Home</li>
   <li id="menu">Menu</li>
   <li id="contact">Contact</li>
 </ul>`;
-  nav.classList.add("hide");
-  document.body.insertBefore(nav, document.getElementById("content"));
+  nav.classList.add('hide');
+  document.body.insertBefore(nav, document.getElementById('content'));
 
-  const mobileMenu = document.createElement("i");
-  mobileMenu.setAttribute("class", "fas fa-bars");
-  document.body.insertBefore(mobileMenu, document.getElementById("content"));
+  const mobileMenu = document.createElement('i');
+  mobileMenu.setAttribute('class', 'fas fa-bars');
+  document.body.insertBefore(mobileMenu, document.getElementById('content'));
 
-  const exitBtn = document.createElement("i");
-  exitBtn.setAttribute("class", "fas fa-times");
-  exitBtn.classList.add("hide");
-  document.body.insertBefore(exitBtn, document.getElementById("content"));
+  const exitBtn = document.createElement('i');
+  exitBtn.setAttribute('class', 'fas fa-times');
+  exitBtn.classList.add('hide');
+  document.body.insertBefore(exitBtn, document.getElementById('content'));
 
-  exitBtn.addEventListener("click", () => {
-    nav.classList.add("hide");
-    mobileMenu.classList.remove("hide");
-    exitBtn.classList.add("hide");
+  exitBtn.addEventListener('click', () => {
+    nav.classList.add('hide');
+    mobileMenu.classList.remove('hide');
+    exitBtn.classList.add('hide');
   });
 
-  mobileMenu.addEventListener("click", () => {
-    nav.classList.remove("hide");
-    mobileMenu.classList.add("hide");
-    exitBtn.classList.remove("hide");
+  mobileMenu.addEventListener('click', () => {
+    nav.classList.remove('hide');
+    mobileMenu.classList.add('hide');
+    exitBtn.classList.remove('hide');
   });
 };
 
