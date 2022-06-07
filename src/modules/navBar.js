74f3c713135b1ae8c6fcd993e081eb59b6abe7a6
Zero to Mastery <vkormoczy@gmail.com>
2022-06-07 17:58:56 +0300

    Add exit button of mobile menu

--- src/modules/navBar.js
+++ src/modules/navBar.js
@@ -11,6 +11,23 @@ const createNavBar = () => {
   const mobileMenu = document.createElement("i");
   mobileMenu.setAttribute("class", "fas fa-bars");
   document.body.insertBefore(mobileMenu, document.getElementById("content"));
+
+  const exitBtn = document.createElement("i");
+  exitBtn.setAttribute("class", "fas fa-times");
+  exitBtn.classList.add("hide");
+  document.body.insertBefore(exitBtn, document.getElementById("content"));
+
+  exitBtn.addEventListener("click", () => {
+    nav.classList.add("hide");
+    mobileMenu.classList.remove("hide");
+    exitBtn.classList.add("hide");
+  });
+
+  mobileMenu.addEventListener("click", () => {
+    nav.classList.remove("hide");
+    mobileMenu.classList.add("hide");
+    exitBtn.classList.remove("hide");
+  });
 };
 
 export default createNavBar;
